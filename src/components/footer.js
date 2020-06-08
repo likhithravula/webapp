d0ded29fd580caf1d42dc766698937fcba06905b
tombjarne <bjarne.seidel@icloud.com>
2020-06-08 21:13:39 +0200

    Updated landingpage with menu, footer and roadmap section.

---
+++ src/components/footer.js
@@ -0,0 +1,27 @@
+import { inject, observer } from "mobx-react";
+import React from "react";
+import { withRouter } from 'react-router';
+
+import Navbar from "react-bootstrap/Navbar";
+import '../assets/css/Landingpage.css';
+
+class LandingNav extends React.Component {
+
+    render() {
+
+        return (
+            <Navbar id="footer" sticky="bottom">
+                <div className="footer-section">
+                    <h5>Social Media</h5>
+                    <a href="https://www.facebook.com/thedigitalvolunteer/">Facebook</a>
+                    <a href="https://www.instagram.com/thedigitalvolunteer/">Instagram</a>
+                    <a href="https://www.linkedin.com/in/loopdigital/">Linkedin</a>
+                </div>
+            </Navbar>
+        );
+    }
+}
+
+export default withRouter(
+    inject('navigation', 'user')( observer(LandingNav) )
+);
