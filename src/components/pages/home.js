e889973f80fc8f00502f80cd1aa87704367f1f44
tombjarne <bjarne.seidel@icloud.com>
2020-06-10 10:47:33 +0200

    Landingpage content update

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,11 +4,12 @@ import {Link} from "react-router-dom";
 import LandingNav from "../landing-nav";
 import Footer from "../footer";
 import Button from "react-bootstrap/Button";
-import imagePlayButton from "../../assets/img/play.png";
+import ReactPlayer from "react-player";
 
 import personHeart from "../../assets/img/person-heart.svg";
 import handHeart from "../../assets/img/hand-heart.svg";
 import lockHeart from "../../assets/img/lock-heart.svg";
+import chat from "../../assets/img/chat.png"
 
 class Home extends React.Component {
 
@@ -16,8 +17,18 @@ class Home extends React.Component {
 
     return (
       <section id="home">
+        {/* TODO fix navigation */}
         <LandingNav />
         <div id="hero">
+          <div id="vertical-separator"></div>
+          <div id="hero-cta">
+            <h1>Help has arrived!</h1>
+            <p>
+              The digital volunteer helps connecting volunteers with people in need.<br/>
+              Digital, fast and convenient.
+            </p>
+            <Button href="#claim" className="helper-btn">Learn more</Button>
+          </div>
         </div>
         <div id="claim">
           <div className="claim-overlay"></div>
@@ -25,7 +36,7 @@ class Home extends React.Component {
             <h1>The digital volunteer helper</h1>
             <p>
               Volunteer is a tool that connects volunteers with people who need
-              help with getting food, medication and liknande tasks. Click on
+              help with getting food, medication and similar tasks. Click on
               whether you want to help or be helped
             </p>
             <div className="claim-actions">
@@ -41,7 +52,7 @@ class Home extends React.Component {
 
         <div id="home-video">
           <div className="play">
-            <img alt="" src={imagePlayButton}/>
+            <ReactPlayer id="video-player" url="https://www.youtube-nocookie.com/embed/x2y6PHHsJio" />
           </div>
         </div>
 
@@ -94,21 +105,25 @@ class Home extends React.Component {
             <h2>Roadmap</h2>
             <div className="roadmap-feature-list">
               <div className="roadmap-feature">
+                <img alt ="" className="roadmap-icon" src={chat}/>
                 <h5>Milestone</h5>
                 <h6 className="roadmap-date" >01.01.2020</h6>
                 <div className="separator"></div>
               </div>
               <div className="roadmap-feature">
+                <img alt ="" className="roadmap-icon" src={chat}/>
                 <h5>Milestone</h5>
                 <h6 className="roadmap-date" >02.01.2020</h6>
                 <div className="separator"></div>
               </div>
               <div className="roadmap-feature">
+                <img alt ="" className="roadmap-icon" src={chat}/>
                 <h5>Milestone</h5>
                 <h6 className="roadmap-date" >03.01.2020</h6>
                 <div className="separator"></div>
               </div>
-              <div className="roadmap-feature">
+              <div  className="roadmap-feature">
+                <img alt ="" className="roadmap-icon" src={chat}/>
                 <h5>Milestone</h5>
                 <h6 className="roadmap-date" >04.01.2020</h6>
                 <div className="separator"></div>
