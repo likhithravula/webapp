efbe29d21ed05fab5aea648bf5111beae44723ae
santiagonoguez <santiago.arriaga9@gmail.com>
2020-06-11 10:46:29 -0500

    fix comment

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -28,7 +28,7 @@ class Navigation extends React.Component {
     const { navigation, user } = this.props
 
     // eslint-disable-next-line
-    const { data } = user // quirk, for some reason user is not hydrated unless the data field is extracted; this not work for token.
+    const { data } = user // quirk, for some reason user is not hydrated unless the data field is extracted; this does not work for token.
 
     return (
       <header>
