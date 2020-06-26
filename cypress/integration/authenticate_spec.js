206674f279814e1a237e552f8af0284ffdd2fb4a
tombjarne <bjarne.seidel@icloud.com>
2020-06-26 14:56:57 +0200

    Update navigation
    Enable responsive resize
    Enable menu when logged out
    
    Removed landing-nav component and merged it into navigation
    
    Modified cypress tests to match new structure

--- cypress/integration/authenticate_spec.js
+++ cypress/integration/authenticate_spec.js
@@ -15,17 +15,17 @@ describe('authentication', () => {
         const fetchStub = cy.stub( window, 'fetch' )
 
         fetchStub.withArgs( Cypress.sinon.match(/user[/]auth/) )
-        .as( 'authCall' )
-        .returns( callDeferred.promise )
+            .as( 'authCall' )
+            .returns( callDeferred.promise )
 
         fetchStub.withArgs( Cypress.sinon.match(/user[/]14[/]logout/) )
-        .as( 'logoutCall' )
-        .returns({})
+            .as( 'logoutCall' )
+            .returns({})
       }
     })
 
     cy.get( 'a' ).contains( 'Click here to sign in' )
-    .click()
+        .click()
   })
 
   it('should display the sign in page', () => {
@@ -33,7 +33,7 @@ describe('authentication', () => {
 
     cy.get( 'button' ).contains( 'Next' ).should( 'be.visible' )
 
-    cy.get( '#navigation_bar' ).should( 'not.exist' )
+    cy.get( '#responsive-navbar-nav' ).should( 'not.be.visible' )
   })
 
   it('should log in a volunteer', () => {
@@ -49,9 +49,10 @@ describe('authentication', () => {
     cy.get( '@authCall' ).should( 'be.called' )
 
     cy.get( 'h3' )
-    .should( 'contain', 'Here you can see all people who need help' )
+        .should( 'contain', 'Here you can see all people who need help' )
 
-    cy.get( '#navigation_bar' ).should( 'be.visible' )
+    cy.get( '.navbar-toggler' ).first().click()
+    cy.get( '#responsive-navbar-nav' ).should( 'be.visible' )
   })
 
   it('should display error message', () => {
@@ -68,7 +69,7 @@ describe('authentication', () => {
 
     cy.get( 'h3' ).should( 'contain', 'Sign in' )
     cy.get( 'div.alert-danger' )
-    .should( 'contain', 'Could not authenticate user' )
+        .should( 'contain', 'Could not authenticate user' )
   })
 
   it('should clear error message', () => {
@@ -85,11 +86,11 @@ describe('authentication', () => {
 
     cy.get( 'h3' ).should( 'contain', 'Sign in' )
     cy.get( 'div.alert-danger' ).as( 'message' )
-    .should( 'contain', 'Could not authenticate user' )
-    .click()
+        .should( 'contain', 'Could not authenticate user' )
+        .click()
 
     cy.get( '@message' )
-    .should( 'not.be.visible' )
+        .should( 'not.be.visible' )
   })
 
   describe('Logged in', () => {
@@ -104,47 +105,51 @@ describe('authentication', () => {
       cy.get( 'button' ).contains( 'Next' ).should( 'be.enabled' ).click()
 
       cy.get( 'h3' )
-      .should( 'contain', 'Here you can see all people who need help' )
+          .should( 'contain', 'Here you can see all people who need help' )
 
-      cy.get( '#navigation_bar' ).should( 'be.visible' )
+      cy.get( '#nav' ).should( 'be.visible' )
 
-      cy.get( '#navigation_bar .navbar-toggler-icon' )
-      .should( 'be.visible' )
+      cy.get( '#nav .navbar-toggler' )
+          .should( 'be.visible' )
     })
 
     it('should show menu', () => {
-      cy.get( '#navigation_bar .navbar-toggler-icon' )
-      .click()
+      cy.get( '#nav .navbar-toggler' )
+          .click()
 
-      cy.get( '#navigation_bar a' ).contains( 'Leaderboard' )
-      .should( 'be.visible' )
+      cy.get( '#nav div span' ).contains( 'Login' )
+          .should( 'not.be.visible' )
 
-      cy.get( '#navigation_bar a' ).contains( 'My profile' )
-      .should( 'be.visible' )
+      cy.get( '#nav div span' ).contains( 'Leaderboard' )
+          .should( 'be.visible' )
 
-      cy.get( '#navigation_bar a' ).contains( 'Pending status and history' )
-      .should( 'be.visible' )
+      cy.get( '#nav div span' ).contains( 'My profile' )
+          .should( 'be.visible' )
 
-      cy.get( '#navigation_bar a' ).contains( 'Rate' )
-      .should( 'be.visible' )
+      cy.get( '#nav div span' ).contains( 'Pending status and history' )
+          .should( 'be.visible' )
 
-      cy.get( '#navigation_bar a' ).contains( 'Logout' )
-      .should( 'be.visible' )
+      cy.get( '#nav div span' ).contains( 'Rate' )
+          .should( 'be.visible' )
+
+      cy.get( '#nav div span' ).contains( 'Logout' )
+          .should( 'be.visible' )
     })
 
     it('should show menu bar even if the page is refreshed', () => { // here we are testing that user token persistence is working
       cy.visit( `${ TEST_SERVER }/helper/map` )
 
-      cy.get( '#navigation_bar' ).should( 'be.visible' )
+      cy.get( '#nav' ).should( 'be.visible' )
     })
 
     it('should log out a volunteer', () => {
-      cy.get( '#navigation_bar .navbar-toggler-icon' )
-      .click()
 
-      cy.get( '#navigation_bar a' ).contains( 'Logout' ).click()
+      cy.get( '#nav .navbar-toggler' )
+          .click()
 
-      cy.get( '#navigation_bar' ).should( 'not.exist' )
+      cy.get( '#nav div span' ).contains( 'Logout' ).click()
+
+      cy.get( '.mr-auto' ).first().should( 'not.be.visible' )
 
       cy.get( 'h1' ).should( 'contain', 'The digital volunteer helper' )
     })
