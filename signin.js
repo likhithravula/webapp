2b39e49c6456b3c6bd20e0d8c5f1e8e1bb34bf54
Suneetha <33000263+Sunivihaan@users.noreply.github.com>
2020-07-20 17:56:33 +0000

    Add files via upload

---
+++ signin.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Progress from "./progress";
+
+class RegistrationBankID extends React.Component {
+  render() {
+    const current = 3;
+    return (
+      <div className="wrapper">
+        <Progress current={current}/>
+        <section id="registration_signin">
+          <div className="text-box">
+            <h3>Welcome</h3>
+          </div>
+
+          <Form>
+            <Form.Group>
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="text" placeholder="Enter your Email"/>
+            </Form.Group>
+          </Form>
+		  
+		  <Form>
+		  <Form.Group>
+              <Form.Label>Create Password</Form.Label>
+              <Form.Control type="text" placeholder="**********"/>
+            </Form.Group>
+          </Form>
+		  
+		  <Form>
+		  <Form.Group>
+              <Form.Label>What is you Favorite Color</Form.Label>
+              <Form.Control type="text" placeholder="This will be your Password Hint"/>
+            </Form.Group>
+          </Form>
+
+
+
+          <div className="mt-4">
+            <Link to="/registration/information">
+              <Button className="btn btn-block helper-btn">
+                SignUp
+              </Button>
+            </Link>
+          </div>
+
+        </section>
+      </div>
+    );
+  }
+}
+
+export default RegistrationBankID;
---
+++ terms.js
@@ -0,0 +1,78 @@
+import { inject, observer } from "mobx-react"
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Progress from "./progress";
+
+class RegistrationTerms extends React.Component {
+  pageId;
+
+  constructor( props ) {
+    super( props )
+    this.pageId = 1;
+    this.changeTermsOfServiceFlag = this.changeTermsOfServiceFlag.bind( this )
+  }
+
+  changeTermsOfServiceFlag(event) {
+    const { registration } = this.props;
+
+    registration.termsOfServiceAccepted = event.target.checked
+  }
+
+  render() {
+    const { registration } = this.props;
+    const current = 1;
+
+    return (
+      <div className="wrapper">
+        <Progress current={current}/>
+        <section id="registration_terms">
+          <div className="text-box">
+            <h3>Terms and conditions</h3>
+          </div>
+
+          <h2>What will we collect?</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+          </p>
+
+          <h2>Do you want to know more?</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. <a href="#more">Read More ></a>
+          </p>
+
+          <Form>
+            <Form.Group>
+              <Form.Check type="checkbox" name="termsOfService"
+                label="I agree to the terms and conditions"
+                checked={ registration.termsOfServiceAccepted }
+                onClick={ this.changeTermsOfServiceFlag }
+              />
+            </Form.Group>
+          </Form>
+
+          <div className="mt-4">
+            <Link to="/registration/alternatives">
+              <Button className="btn btn-block helper-btn"
+                disabled={ !registration.termsOfServiceAccepted }
+              >
+                Create My Account
+              </Button>
+            </Link>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default inject('registration')( observer(RegistrationTerms) );
