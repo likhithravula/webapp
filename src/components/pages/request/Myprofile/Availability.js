6c08719c03994c029e05c12c362cf07f807886eb
Jaya Krishna <jayakrishnauppalapati99@gmail.com>
2020-07-24 23:02:21 +0530

    Completed update availability

--- src/components/pages/request/Myprofile/Availability.js
+++ src/components/pages/request/Myprofile/Availability.js
@@ -9,18 +9,18 @@ const Availability = () => {
     const [isFri, setIsFri] = useState(false);
     const [isSat, setisSat] = useState(false);
     const [isSun, setisSun] = useState(false);
-    const [istime9, setistime9] = useState(false);
-    const [istime10, setistime10] = useState(false);
-    const [istime11, setistime11] = useState(false);
-    const [istime12, setistime12] = useState(false);
-    const [istime13, setistime13] = useState(false);
-    const [istime14, setistime14] = useState(false);
-    const [istime15, setistime15] = useState(false);
-    const [istime16, setistime16] = useState(false);
-    const [istime17, setistime17] = useState(false);
-    const [istime18, setistime18] = useState(false);
-    const [istime19, setistime19] = useState(false);
-    const [istime20, setistime20] = useState(false);
+    const [istimen, setistimen] = useState(false);
+    const [istimet, setistimet] = useState(false);
+    const [istimeel, setistimeel] = useState(false);
+    const [istimetw, setistimetw] = useState(false);
+    const [istimethi, setistimethi] = useState(false);
+    const [istimefo, setistimefo] = useState(false);
+    const [istimefi, setistimefi] = useState(false);
+    const [istimesi, setistimesi] = useState(false);
+    const [istimese, setistimese] = useState(false);
+    const [istimeei, setistimeei] = useState(false);
+    const [istimeni, setistimeni] = useState(false);
+    const [istimetwe, setistimetwe] = useState(false);
     const [isWeekdays, setIsWeekdays] = useState(false);
     const [isWeekends, setisWeekends] = useState(false);
     const [isAllday, setisAllday] = useState(false)
@@ -57,32 +57,36 @@ const Availability = () => {
                     }}>Weekends</button> 
             </div>
             <div>
-                <button className='mon' id='daily' style={{ background: isMon ? '#5FD680' : null }} onClick={()=>{setIsMon(!isMon)}}>Mon</button>
-                <button className='tue' id='daily' style={{ background: isTue ? '#5FD680' : null }} onClick={()=>{setIsTue(!isTue)}}>Tue</button>
-                <button className='wed' id='daily' style={{ background: isWed ? '#5FD680' : null }} onClick={()=>{setIsWed(!isWed)}}>Wed</button>
-                <button className='thu' id='daily' style={{ background: isThu ? '#5FD680' : null }} onClick={()=>{setIsThu(!isThu)}}>Thu</button>
-                <button className='fri' id='daily' style={{ background: isFri ? '#5FD680' : null }} onClick={()=>{setIsFri(!isFri)}}>Fri</button>
-                <button className='sat' id='daily' style={{ background: isSat ? '#5FD680' : null }} onClick={()=>{setisSat(!isSat)}}>Sat</button>
-                <button className='sun' id='daily' style={{ background: isSun ? '#5FD680' : null }} onClick={()=>{setisSun(!isSun)}}>Sun</button>
+                <button className='mon' id='daily' style={{ background: isMon ? '#5FD680' : null }} onClick={()=>  {setIsMon(!isMon) }}>Mon</button>
+                <button className='tue' id='daily' style={{ background: isTue ? '#5FD680' : null }} onClick={()=>  {setIsTue(!isTue) }}>Tue</button>
+                <button className='wed' id='daily' style={{ background: isWed ? '#5FD680' : null }} onClick={()=>  {setIsWed(!isWed) }}>Wed</button>
+                <button className='thu' id='daily' style={{ background: isThu ? '#5FD680' : null }} onClick={()=>  {setIsThu(!isThu) }}>Thu</button>
+                <button className='fri' id='daily' style={{ background: isFri ? '#5FD680' : null }} onClick={()=>  {setIsFri(!isFri) }}>Fri</button>
+                <button className='sat' id='daily' style={{ background: isSat ? '#5FD680' : null }} onClick={()=>  {setisSat(!isSat) }}>Sat</button>
+                <button className='sun' id='daily' style={{ background: isSun ? '#5FD680' : null }} onClick={()=>  {setisSun(!isSun) }}>Sun</button>
             </div>
             <div>
                 <button  className='All' id={isAllday? 'all-day-selected' : 'all-day' } onClick={() => setisAllday(!isAllday)}>All day</button>
             </div>
             <div>
-                <button id={istime9? 'time-selected' : 'time'} className='time_nine'>9:00-10:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_ten'>10:00-11:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_eleven'>11:00-12:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_twelve'>12:00-13:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_thirteen'>13:00-14:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_fourteen'>14:00-15:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_fifteen'>15:00-16:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_sixteen'>16:00-17:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_seventeen'>17:00-18:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_eighteen'>18:00-19:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_nineteen'>19:00-20:00</button>
-                <button id={istime? 'time-selected' : 'time'} className='time_twenty'>20:00-21:00</button>
+                <button onClick={() => {setistimen(!istimen)}} id={istimen? 'time-selected' : 'time'} className='time_nine'>9:00-10:00</button>
+                <button onClick={() => {setistimet(!istimet)}} id={istimet? 'time-selected' : 'time'} className='time_ten'>10:00-11:00</button>
+                <button onClick={() => {setistimeel(!istimeel)}} id={istimeel? 'time-selected' : 'time'} className='time_eleven'>11:00-12:00</button>
+                <button onClick={() => {setistimetw(!istimetw)}} id={istimetw? 'time-selected' : 'time'} className='time_twelve'>12:00-13:00</button>
+                <button onClick={() => {setistimethi(!istimethi)}} id={istimethi? 'time-selected' : 'time'} className='time_thirteen'>13:00-14:00</button>
+                <button onClick={() => {setistimefo(!istimefo)}} id={istimefo? 'time-selected' : 'time'} className='time_fourteen'>14:00-15:00</button>
+                <button onClick={() => {setistimefi(!istimefi)}} id={istimefi? 'time-selected' : 'time'} className='time_fifteen'>15:00-16:00</button>
+                <button onClick={() => {setistimesi(!istimesi)}} id={istimesi? 'time-selected' : 'time'} className='time_sixteen'>16:00-17:00</button>
+                <button onClick={() => {setistimese(!istimese)}} id={istimese? 'time-selected' : 'time'} className='time_seventeen'>17:00-18:00</button>
+                <button onClick={() => {setistimeei(!istimeei)}} id={istimeei? 'time-selected' : 'time'} className='time_eighteen'>18:00-19:00</button>
+                <button onClick={() => {setistimeni(!istimeni)}} id={istimeni? 'time-selected' : 'time'} className='time_nineteen'>19:00-20:00</button>
+                <button onClick={() => {setistimetwe(!istimetwe)}} id={istimetwe? 'time-selected' : 'time'} className='time_twenty'>20:00-21:00</button>
+            </div>
+            <div className='save'>
+                <button className='save-availability'>Save availability</button>
             </div>
         </div>
+
 
     )
     // }
