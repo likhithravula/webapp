4a0779498c41d65ece9b8d9512fae757a415e344
Jaya Krishna <jayakrishnauppalapati99@gmail.com>
2020-07-27 10:09:11 +0530

    Completed Hero-page

--- src/components/pages/request/Myprofile/Heroprofile.js
+++ src/components/pages/request/Myprofile/Heroprofile.js
@@ -4,6 +4,7 @@ import Driving from './../../../../assets/img/driving.png'
 import Food from './../../../../assets/img/food.png'
 import Med from './../../../../assets/img/med.png'
 import Other from './../../../../assets/img/other.png'
+import Vector from './../../../../assets/img/Vector.svg'
 import './../../../../assets/css/App.css'
 
 
@@ -11,7 +12,8 @@ export class Hero extends Component {
     render() {
         return (
             <div>
-                <div className = 'profile-bg-hero'>
+                <div className = 'profile-bg-hero'> 
+                   <img src = {Vector}  className = 'vector-hero_icon'/>
                    <h1 className='h1-hero'>How do you like to help</h1>
                 </div>
                 
@@ -43,10 +45,10 @@ export class Hero extends Component {
                     <Button className = 'hero-btn'>I can do this!</Button>
                 </div>
                 <di>
-                    <img src= {Food}/>
-                    <img src={Driving} />
-                    <img src={Med}/>
-                    <img src= {Other}/>
+                    <img className = 'food-hero_icon' src= {Food} />
+                    <img className = 'driving-hero_icon' src={Driving} />
+                    <img className = 'med-hero_icon' src={Med}/>
+                    <img className = 'others-hero_icon' src= {Other}/>
                 </di>
             </div>
         )
